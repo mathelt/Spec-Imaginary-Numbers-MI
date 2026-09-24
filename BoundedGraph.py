54deb4f082ca26acf0c6edf28755a10f9ab80448
Luke M <mathelt@pedarecc.sa.edu.au>
2026-09-24 02:52:33 +0000

    Adding a compiler for all the bounded numbers

--- BoundedGraph.py
+++ BoundedGraph.py
@@ -2,10 +2,14 @@ isBounded = 0
 zNought = 0
 zN = zNought
 
-realPart = -2
-complexPart = 0j
+realPart = 0.6
+complexPart = 0.4j
 c = realPart + complexPart
 print(c)
+
+x = []
+y = []
+
 
 # iteratively calculate the next term of z_n and determine if the modulus exceeds two, returning whether it is bounded or not.
 def CalculateZn1(zN, c, isBounded, iterations):
@@ -34,10 +38,23 @@ def testNumberGenerator():
         y.append(imaginary)
         imaginary = round((imaginary + 0.01), 5)
 
-def createBoundedArray():
-    #Test
-    print()
+    return x, y
+
+def createBoundedArray(x,y):
+    a = 0
+    boundedArray = []
+    for i in range(len(x)):
+        #Imaginary number issue
+        complexNumber = complex(x[i], y[a])
+        CalculateZn1(0, complexNumber, 0, 100)
+        if isBounded == True:
+            boundedArray.append(complexNumber)
+    #this is not printing more than the [] which makes me think that the complex numbers are not appending themselves
+    print(boundedArray)
 
 #program run
-print(CalculateZn1(0, c, 0, 20))
-testNumberGenerator()
+#print(CalculateZn1(0, c, 0, 20))
+
+x, y = testNumberGenerator()
+
+createBoundedArray(x,y)
